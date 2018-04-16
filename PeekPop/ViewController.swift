ed59317aaad6935708b4365b8b04cd8fb57a2454
Neftali Samarey <neftalisamarey@neftalis-macbook-pro.local>
2018-04-16 13:21:14 -0400

    Initial Commit for Peek and Pop iOS app sample

--- PeekPop/ViewController.swift
+++ PeekPop/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIViewControllerPreviewingDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if (traitCollection.forceTouchCapability == UIForceTouchCapability.available) {
+            registerForPreviewing(with: self, sourceView: view)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: Delegates
+    
+    // Handle the Previewing
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        // Needs to return a UIViewController
+        let previewController = PreviewViewController()
+        return previewController
+    }
+    
+    // Handle the transition
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+        let final = FinalViewController()
+        show(final, sender: self)
+    }
 
 }
 
